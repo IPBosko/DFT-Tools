d479364d70ccfe1525ad3488bb863eaa40e87e74
Ivan Bosko <ivan.p.bosko@gmail.com>
2026-03-03 13:06:14 -0500

    Added sic_tf.py

--- LPS/src/sic_tf.py
+++ LPS/src/sic_tf.py
@@ -1,11 +1,29 @@
-def sic_p(N):
-    return (1 - (2 / N) ** (2 / 3))
+def sic_bosko(N):
+    """
+    A closed-shell self-interaction correction
+    to Thomas-Fermi functional derived analytically by Bosko
+    """
+    return 1 - (2 / N) ** (2 / 3)
 
-def sic_p_absp(N):
-    return (1 - (2 / N) ** (1 / 3))
+def sic_absp1(N):
+    """
+    A closed-shell self-interaction correction to Thomas-Fermi
+    functional derived in Acharya et al, 1980. Coefficient 
+    C_ABSP1 = 1.412 is obtained by fitting to 55 HF neutral atoms
+    """
+    return 1 - 1.412 * N ** ( - 1 / 3 )
 
-def sic_x(N):
-    return (1 - (2 / N) ** (1 / 3))
+def sic_absp2(N):
+    """
+    A closed-shell self-interaction correction to Thomas-Fermi
+    functional derived in Acharya et al, 1980. Coefficient 
+    C_ABSP2 = 1.332 is obtained by fitting to 1200 HF atoms and ions
+    """
+    return 1 - 1.332 * N ** ( - 1 / 3 )
 
-def sic_p_test(N):
-    return (1 - (1.412 * (N) ** (- 1 / 3)))
+def sic_gr(N):
+    """
+    A closed-shell self-interaction correction to Thomas-Fermi
+    functional derived by Gazquez and Robles in 1982 (Eq.54) 
+    """
+    return (1 - 2 / N) * (1 - 1.015 * N ** ( - 1 / 3 ) + 0.150 * N ** ( - 2 / 3 ))
